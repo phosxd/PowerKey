4833e0f6e045be1965955bfcd0c91c06e203de36
phosxd <phosphorusxd@proton.me>
2025-05-02 01:12:15 -0400

    Tiny fix.

--- addons/PowerKey/Editor/Editor Inspector/PKExpression Editor.gd
+++ addons/PowerKey/Editor/Editor Inspector/PKExpression Editor.gd
@@ -46,8 +46,14 @@ func _on_dropdown_button_down() -> void:
 
 
 func _on_text_editor_text_changed() -> void:
+	# Update current PKExpressions.
 	PKExpressions = %'Text Editor'.text
 	on_update.emit(PKExpressions)
+	
+	# Ensure dropdown is expanded when typing.
+	if not expanded:
+		_on_dropdown_button_down()
+	
 	# Reset line color for first line.
 	%'Text Editor'.set_line_background_color(0, Color(0,0,0,0))
 	# Validate expressions, if not empty.
--- addons/PowerKey/Scripts/EditorInspectorPlugin.gd
+++ addons/PowerKey/Scripts/EditorInspectorPlugin.gd
@@ -5,6 +5,7 @@ const PKExpression_Editor := preload('res://addons/PowerKey/Editor/Editor Inspec
 func _can_handle(object:Object) -> bool:
 	return true if object is Node else false # Return true if is a Node.
 
+
 func _parse_category(object:Object, category:String) -> void:
 	if category == 'Node':
 		# Create PKExpression Editor instance & initialize it.
@@ -12,8 +13,10 @@ func _parse_category(object:Object, category:String) -> void:
 		var pk_expressions = object.get_meta('PKExpressions', false)
 		if pk_expressions:
 			PKExpEditor_instance.init(pk_expressions)
+			
 		# On PKExp Editor sends update signal, update the Node.
 		PKExpEditor_instance.on_update.connect(func(pk_expressions:String) -> void:
+			# NOTE: Adding or removing metadata modifies Inspector controls, which closes the PKExpEditor dropdown. This is sort-of counteracted in the PKExpEditor Script.
 			# If not empty, set meta.
 			if pk_expressions.length() > 0:
 				object.set_meta('PKExpressions', pk_expressions)
@@ -21,5 +24,6 @@ func _parse_category(object:Object, category:String) -> void:
 			else:
 				object.remove_meta('PKExpressions')
 		)
+		
 		# Add PKExpression Editor to the inspector for this Node.
 		self.add_custom_control(PKExpEditor_instance)
